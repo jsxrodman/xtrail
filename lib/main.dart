32c1fc3b51515cc3cde4dd57a4b4c460e2e2058a
justixr <justixrodman@gmail.com>
2020-07-15 00:22:52 +0300

    Added auth page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:xtrail/activities/init_app.dart';
+import 'package:xtrail/services/authentication.dart';
+import 'package:xtrail/pages/root_page.dart';
 
+void main() {
+  runApp(new MyApp());
+}
 
-void main() => runApp(
-  new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: new StartAppInitActivity(),
-  ),
-);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
